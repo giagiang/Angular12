0cbe380daf617ccef733d7a2edb6028cb77a5193
fastboy <giagiang200@gmail.com>
2022-01-12 11:34:27 +0700

    Data Baiding Value

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public name = 'Lê Gia Giang';
+
   constructor() { }
 
   ngOnInit(): void {
